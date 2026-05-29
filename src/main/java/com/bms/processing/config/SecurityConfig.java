565e540e34e5a9aa67edbebf947ee1b5ce546d86
jtepson <johnepperson1997@gmail.com>
2026-05-29 13:10:00 -0500

    Removed depracated vaadin security for VaadinSecurityConfigurer for V25

--- src/main/java/com/bms/processing/config/SecurityConfig.java
+++ src/main/java/com/bms/processing/config/SecurityConfig.java
@@ -1,19 +1,27 @@
 package com.bms.processing.config;
 
-import com.vaadin.flow.spring.security.VaadinWebSecurity;
+import com.vaadin.flow.spring.security.VaadinSecurityConfigurer;
+import com.vaadin.flow.spring.security.VaadinAwareSecurityContextHolderStrategyConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-public class SecurityConfig extends VaadinWebSecurity {
+@EnableWebSecurity
+@Import(VaadinAwareSecurityContextHolderStrategyConfiguration.class)
+public class SecurityConfig {
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
-        http.oauth2Login(oauth2 -> oauth2
-                .defaultSuccessUrl("/", true)
+        http.with(
+                VaadinSecurityConfigurer.vaadin(),
+                vaadin -> vaadin.oauth2LoginPage("/oauth2/authorization/keycloak")
         );
 
-        super.configure(http);
+        return http.build();
     }
 }
