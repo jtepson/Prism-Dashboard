5b0b95264a2bb663a8abbe7705c952f7642eb1e2
jtepson <johnepperson1997@gmail.com>
2026-06-08 13:59:30 -0500

    Added wiring to services for notifications and recipients for emailing groups

--- src/main/java/com/bms/processing/service/EmailService.java
+++ src/main/java/com/bms/processing/service/EmailService.java
@@ -4,6 +4,8 @@ import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class EmailService {
 
@@ -29,6 +31,32 @@ public class EmailService {
         SimpleMailMessage message = new SimpleMailMessage();
 
         message.setTo(to);
+        message.setFrom("support@prismclinicalimaging.com");
+        message.setSubject(subject);
+        message.setText(body);
+
+        mailSender.send(message);
+    }
+
+    // single email with TO + CC recipients
+    public void sendEmail(
+            List<String> toRecipients,
+            List<String> ccRecipients,
+            String subject,
+            String body
+    ) {
+        SimpleMailMessage message = new SimpleMailMessage();
+
+        message.setTo(
+                toRecipients.toArray(new String[0])
+        );
+
+        if (!ccRecipients.isEmpty()) {
+            message.setCc(
+                    ccRecipients.toArray(new String[0])
+            );
+        }
+
         message.setFrom("support@prismclinicalimaging.com");
         message.setSubject(subject);
         message.setText(body);
